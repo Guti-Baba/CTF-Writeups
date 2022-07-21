1bf596f66c7feb4d337a396d11a2fe45a13cd98c
Guti-Baba <79633162+Guti-Baba@users.noreply.github.com>
2022-07-21 21:41:13 +0600

    Add files via upload

---
+++ bdsec-ctf 2022/CryptoCode_solve.py
@@ -0,0 +1,6 @@
+import cryptocode
+
+cipher="c00EtfL9GPq2EItQrkFyPKIMfVFZy0O4ssXtr/V2Io7NMbNS*Brue6Cex4JuWkWU0lUEK2w==*f8EsezuHu2WBstRDlWZiLg==*CZ/4FNMavWZu3kznPrAyeg=="
+key="BDSEC"
+dec= cryptocode.decrypt(cipher,key)
+print(dec)
